28afca27fa28d3949830d1baba248396a77ce8c1
DemetreShonia <shonka.demeloper@gmail.com>
2023-07-06 14:42:45 +0400

    Import New Heli

---
+++ Helicopter/Assets/Intro_Heli_Physics/Code/Scripts/Input/BaseHeliInput.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shonia
+{
+    public class BaseHeliInput : MonoBehaviour
+    {
+        #region Variables
+
+        #endregion
+
+        #region BuiltIn Methods
+        void Update()
+        {
+
+        }
+        #endregion
+        #region Custom Methods
+
+        #endregion
+    } 
+}
