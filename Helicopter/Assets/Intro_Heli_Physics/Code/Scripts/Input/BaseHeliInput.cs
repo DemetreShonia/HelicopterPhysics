a8b75a8bdc9d48a1893fe017c903698ce42e7d4b
DemetreShonia <shonka.demeloper@gmail.com>
2023-07-06 15:18:31 +0400

    Custom Script Templates done

--- Helicopter/Assets/Intro_Heli_Physics/Code/Scripts/Input/BaseHeliInput.cs
+++ Helicopter/Assets/Intro_Heli_Physics/Code/Scripts/Input/BaseHeliInput.cs
@@ -7,17 +7,30 @@ namespace Shonia
     public class BaseHeliInput : MonoBehaviour
     {
         #region Variables
+        [Header("Base Input Properties")]
+        [SerializeField] float _vertical = 0f;
+        [SerializeField] float _horizontal = 0f;
+
+        #region Strings
+        const string VERTICAL = "Vertical";
+        const string HORIZONTAL = "Horizontal";
+
+        #endregion
 
         #endregion
 
         #region BuiltIn Methods
         void Update()
         {
-
+            HandleInputs();
         }
         #endregion
         #region Custom Methods
-
+        protected virtual void HandleInputs() 
+        {
+            _vertical = Input.GetAxis(VERTICAL);
+            _horizontal = Input.GetAxis(HORIZONTAL);
+        }
         #endregion
     } 
 }
---
+++ Helicopter/Assets/Intro_Heli_Physics/Code/Scripts/Input/KeyBoardHeliInput.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Shonia
+{
+	public class KeyBoardHeliInput : MonoBehaviour
+	{
+		#region Variables
+		
+		#endregion
+		
+		#region Builtin Methods
+		void Start()
+		{
+		
+		}
+		
+		void Update()
+		{
+		
+		}
+		#endregion
+		
+		#region Custom methods
+		
+		#endregion
+	}
+}
